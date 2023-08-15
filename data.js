707e8aed8665e457df4d21e477e336e81b99d9a5
Abitoftrolling <emmanuelo@seoscholars.org>
2023-08-15 16:00:50 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,71 @@
-const StoreA = [
+const Bestbuy = [
     {
-        name: ""
-        brand:
-        price:
-        quality: (new, used, refurbished)
-        color:
-    } 
+        name: "iphone14 pro",
+        brand: "apple",
+        price: "999",
+        quality: "new",
+        color: "black"
+    },
+
+    {
+        name: "air force 1s",
+        brand: "nike",
+        price: "100",
+        quality: "new",
+        color: "red"
+    },
+
+    {
+        name: "playstation 5",
+        brand: "sony",
+        price: "475",
+        quality: "new",
+        color: "white"
+    },
+
+    {
+        name: "backpack",
+        brand: "jansport",
+        price: "60",
+        quality: "new",
+        color: "black"
+    }
 ];
 
-const StoreB = [{
-    
-}
+const Walmart = [
+    {
+        name: "iphone14 pro",
+        brand: "apple",
+        price: "899",
+        quality: "used",
+        color: "red",
+    },
 
-]
+    {
+        name: "air force 1s",
+        brand: "nike",
+        price: "110",
+        quality: "new",
+        color: "white",
+
+    },
+    
+    {
+        name: "playstation 5",
+        brand: "sony",
+        price: "550",
+        quality: "new",
+        color: "white",
+    },
+    {
+        name: "backpack",
+        brand: "jansport",
+        price: "53",
+        quality: "referbreshed",
+        color: "grey",
+    }
+
+];
 
 
 
@@ -21,7 +74,6 @@ const StoreB = [{
 
 roducts.forEach(StoreA => {
     const StoreADiv = document.createElement('div');
-    StoreADiv.className = 'store';
     StoreADiv.innerHTML = `
       <div class="product-info">
         <h2>${StoreA.name}</h2>
@@ -31,4 +83,4 @@ roducts.forEach(StoreA => {
       <div class="brand">$${storeA.brand}</div>
       <div class= "color">$${storeA.color}</div>
     `;
-
+}
