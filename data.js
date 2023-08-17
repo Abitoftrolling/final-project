8ebae0de2201069a08d1e3697696d1487941271b
Abitoftrolling <emmanuelo@seoscholars.org>
2023-08-16 20:21:27 -0400

    complete

--- data.js
+++ data.js
@@ -1,57 +1,59 @@
 const Bestbuy = [
     {
-        name: "iphone14 pro",
-        brand: "apple",
+        name: "iPhone 14 Pro",
+        brand: "Apple",
         price: "999",
-        quality: "new",
-        color: "black"
+        quality: "New",
+        color: "Black"
     },
 
     {
-        name: "air force 1s",
-        brand: "nike",
+        name: "Air Force 1s",
+        brand: "Nike",
         price: "100",
-        quality: "new",
-        color: "red"
+        quality: "New",
+        color: "Red"
     },
 
     {
-        name: "playstation 5",
-        brand: "sony",
+        name: "Playstation 5",
+        brand: "Sony",
         price: "475",
-        quality: "new",
-        color: "white"
+        quality: "New",
+        color: "White"
     },
 
     {
-        name: "backpack",
-        brand: "jansport",
+        name: "Backpack",
+        brand: "Jansport",
         price: "60",
-        quality: "new",
+        quality: "New",
         color: "black"
     },
 
     {
-        name: "airpod pros 2nd gen",
-        brand: "apple",
+        name: "Airpods Pro (2nd gen)",
+        brand: "Apple",
         price: "250",
-        quality: "new",
+        quality: "New",
         color: "white"
     }
 ];
 
 const Walmart = [
     {
-        name: "iphone14 pro",
-        brand: "apple",
+        name: "iPhone 14 Pro",
+        image: "images/phone.png",
+        brand: "Apple",
         price: "899",
         quality: "used",
-        color: "red",
+        color: "Black",
     },
 
     {
-        name: "air force 1s",
-        brand: "nike",
+        name: "Air Force 1s",
+        image: "images/shoes.png",
+        brand: "Nike",
         price: "110",
         quality: "new",
         color: "white",
@@ -59,24 +61,27 @@ const Walmart = [
     },
     
     {
-        name: "playstation 5",
-        brand: "sony",
+        name: "Playstation 5",
+        image: "images/PS5.png",
+        brand: "Sony",
         price: "550",
         quality: "new",
         color: "white",
     },
 
     {
-        name: "backpack",
-        brand: "jansport",
+        name: "Backpack",
+        image: "images/backpack.png",
+        brand: "Jansport",
         price: "53",
         quality: "refurbished",
         color: "grey",
     },
     
     {
-        name: "airpod pros 2nd gen",
-        brand: "apple",
+        name: "Airpods Pro (2nd gen)",
+        image: "images/airpods.png",
+        brand: "Apple",
         price: "199",
         quality: "new",
         color: "white"
@@ -97,11 +102,12 @@ for (let i = 0; i < Walmart.length; i++) {
     
     productDiv.innerHTML = `
       <div class="product-info">
-        <h2>${walmartProduct.name}</h2>
-        <p>Brand: ${walmartProduct.brand}</p>
-        <p>Price - Walmart: $${walmartProduct.price} | Best Buy: $${bestbuyProduct.price}</p>
-        <p>Quality - Walmart: ${walmartProduct.quality} | Best Buy: ${bestbuyProduct.quality}</p>
-        <p>Color - Walmart: ${walmartProduct.color} | Best Buy: ${bestbuyProduct.color}</p>
+        <div><h2>${walmartProduct.name}</h2></div>
+        <div><img src="${walmartProduct.image}"></div>
+        <div><p>Brand: ${walmartProduct.brand}</p></div>
+        <div><p>Price - Walmart: $${walmartProduct.price} | Best Buy: $${bestbuyProduct.price}</p></div>
+        <div><p>Quality - Walmart: ${walmartProduct.quality} | Best Buy: ${bestbuyProduct.quality}</p></div>
+        <div><p>Color - Walmart: ${walmartProduct.color} | Best Buy: ${bestbuyProduct.color}</p></div>
       </div>
       <div class="price">
         <button class="opinion-button">Get Opinion</button>
@@ -115,13 +121,17 @@ for (let i = 0; i < Walmart.length; i++) {
     opinionButton.addEventListener('click', function () {
         if (opinionDiv.textContent) {
             opinionDiv.textContent = ''; // Clear the opinion text
-            
         } else {
-            const newOpinion = (cheaperBrand === "Walmart")
-                ? "Walmart might be the better option due to its lower price."
-                : "Best Buy might be the better option due to its lower price.";
+            let opinionMessage = '';
 
-            opinionDiv.textContent = newOpinion;
+            const walmartPrice = parseFloat(walmartProduct.price);
+            const bestbuyPrice = parseFloat(bestbuyProduct.price);
+            const priceDifference = Math.abs(walmartPrice - bestbuyPrice).toFixed(2);
+
+            opinionMessage = `You can save $${priceDifference} by choosing the ${cheaperBrand} option.`;
+
+            opinionDiv.textContent = opinionMessage;
+
             // Close other open opinions
             const allOpinionDivs = document.querySelectorAll('.opinion');
             allOpinionDivs.forEach(div => {
